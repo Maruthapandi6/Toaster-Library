9c9cfe39e8dd5dcce157ad18ee904fd1d334629b
maruthapandi <k.maruthapandi@rifluxyss.com>
2022-04-25 15:54:20 +0530

    testcommit

--- toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
+++ toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.widget.Toast
 
 class ToasterMessage {
-
     fun show(context : Context,message : String)
     {
         Toast.makeText(context,message,Toast.LENGTH_LONG).show()
